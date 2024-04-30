0e4a99655c236d0adaf6a1d813bb54daa902395b
akbar401t <167452422+akbar401t@users.noreply.github.com>
2024-04-30 19:02:50 +0000

    copilot third commit -2

--- comments.js
+++ comments.js
@@ -1,55 +1,57 @@
 //create web server
+//create a server
 var http = require('http');
 var url = require('url');
+var querystring = require('querystring');
 var fs = require('fs');
-var qs = require('querystring');
-var jsonfile = require('jsonfile');
-var file = 'comments.json';
-var comments = jsonfile.readFileSync(file);
+var server = http.createServer(function(req, res) {
+    var postData = '';
+    req.on('data', function(chunk) {
+        postData += chunk;
+    });
 
-//create server
-http.createServer(function (req, res) {
-    var q = url.parse(req.url, true);
-    var filename = "." + q.pathname;
-    var url_parts = url.parse(req.url, true);
-    var query = url_parts.query;
-    var method = req.method;
-
-    if (filename == './comments.html') {
-        fs.readFile(filename, function (err, data) {
-            if (err) {
-                res.writeHead(404, { 'Content-Type': 'text/html' });
-                return res.end("404 Not Found");
-            }
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.write(data);
-            return res.end();
-        });
-    }
-
-    //if the request is post
-    if (method === 'POST') {
-        var body = '';
-        req.on('data', function (data) {
-            body += data;
-        });
-        req.on('end', function () {
-            var post = qs.parse(body);
-            comments.push({ name: post.name, comment: post.comment });
-            jsonfile.writeFileSync(file, comments);
-            res.writeHead(302, { 'Location': 'http://localhost:8080/comments.html' });
-            res.end();
-        });
-    }
-
-    //if the request is get
-    if (method === 'GET') {
-        if (filename == './comments') {
-            res.writeHead(200, { 'Content-Type': 'application/json' });
-            res.write(JSON.stringify(comments));
-            res.end();
+    req.on('end', function() {
+        var pathname = url.parse(req.url).pathname;
+        var query = url.parse(req.url).query;
+        if (pathname === '/comment') {
+            var comment = querystring.parse(postData).comment;
+            fs.appendFile('comments.txt', comment + '\n', function(err) {
+                if (err) {
+                    res.end('error');
+                } else {
+                    res.end('success');
+                }
+            });
+        } else {
+            fs.readFile('comments.txt', function(err, data) {
+                if (err) {
+                    res.end('error');
+                } else {
+                    res.end(data);
+                }
+            });
         }
-    }
-
-}).listen(8080);
-console.log('Server running at http://localhost:8080/');
+    });
+});
+server.listen(3000, function() {
+    console.log('server is running at port 3000');
+});
+//run the server and open the browser and type http://localhost:3000/comments.txt
+//enter some comments
+//open the file comments.txt and check the comments
+//open the browser and type http://localhost:3000/comment?comment=hello
+//check the comments.txt file and check the comments
+//open the browser and type http://localhost:3000/comment?comment=how%20are%20you
+//check the comments.txt file and check the comments
+//open the browser and type http://localhost:3000/comment?comment=good
+//check the comments.txt file and check the comments
+//open the browser and type http://localhost:3000/comment?comment=bad
+//check the comments.txt file and check the comments
+//open the browser and type http://localhost:3000/comment?comment=ok
+//check the comments.txt file and check the comments
+//open the browser and type http://localhost:3000/comment?comment=bye
+//check the comments.txt file and check the comments
+//open the browser and type http://localhost:3000/comment?comment=goodbye
+//check the comments.txt file and check the comments
+//open the browser and type http://localhost:3000/comment?comment=goodnight
+//check
